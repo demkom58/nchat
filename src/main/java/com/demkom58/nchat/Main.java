e3e71ad72fd588217e425fe6cf58c6421de76803
DEMKOM58 <demkom58@gmail.com>
2020-02-06 10:34:15 +0200

    Reduced boilerplate code in Main class of union project

--- src/main/java/com/demkom58/nchat/Main.java
+++ src/main/java/com/demkom58/nchat/Main.java
@@ -1,36 +1,18 @@
 package com.demkom58.nchat;
 
-import com.demkom58.nchat.client.Client;
-import com.demkom58.nchat.server.Server;
 import joptsimple.OptionParser;
-import joptsimple.OptionSet;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         final OptionParser optionParser = new OptionParser();
-
         optionParser.acceptsAll(Arrays.asList("s", "server"));
-        optionParser.acceptsAll(Arrays.asList("h", "host")).availableIf("server").withRequiredArg();
-        optionParser.acceptsAll(Arrays.asList("p", "port")).availableIf("server").withRequiredArg();
-        optionParser.accepts("help").forHelp();
 
-        final OptionSet optionSet = optionParser.parse(args);
-
-        if(optionSet.has("server"))
-            startServer(optionSet);
+        if (optionParser.parse(args).has("server"))
+            com.demkom58.nchat.server.Main.main(args);
         else
-            startClient(optionSet);
-
-    }
-
-    private static void startClient(OptionSet optionSet) {
-        Client.start(optionSet);
-    }
-
-    private static void startServer(OptionSet optionSet) throws Exception {
-        Server.start(optionSet);
+            com.demkom58.nchat.client.Main.main(args);
     }
 
 }
