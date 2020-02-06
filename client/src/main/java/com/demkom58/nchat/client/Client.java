53d3b27d26c68cd82f5e55c86327093c3317eba0
DEMKOM58 <demkom58@gmail.com>
2020-02-06 10:04:42 +0200

    Now project uses gradle sub projects

---
+++ client/src/main/java/com/demkom58/nchat/client/Client.java
@@ -0,0 +1,106 @@
+package com.demkom58.nchat.client;
+
+import com.demkom58.divine.gui.GuiManager;
+import com.demkom58.divine.gui.GuiStyleLoader;
+import com.demkom58.nchat.client.gui.ChatController;
+import com.demkom58.nchat.client.gui.ListController;
+import com.demkom58.nchat.client.gui.LoginController;
+import com.demkom58.nchat.client.network.User;
+import com.demkom58.nchat.common.Environment;
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import io.netty.util.internal.logging.Slf4JLoggerFactory;
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+import joptsimple.OptionSet;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Objects;
+
+public class Client extends Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger("[Client]");
+    private static final String STYLES_PATH = Environment.DATA_PATH + "styles/";
+
+    private static Client client;
+
+    private GuiStyleLoader styleLoader = new GuiStyleLoader();
+    private GuiManager guiManager;
+    private User user;
+
+    @Override
+    public void start(@NotNull final Stage stage) throws Exception {
+        InternalLoggerFactory.setDefaultFactory(Slf4JLoggerFactory.INSTANCE);
+        Client.client = this;
+
+        guiManager = new GuiManager(stage);
+        final File[] files = new File(STYLES_PATH).listFiles();
+
+        if (!styleLoader.checkStyleVersion(Environment.DATA_PATH, Environment.STYLING_VERSION) && files != null)
+            for (File file : files) file.delete();
+
+        styleLoader.exportResource(Environment.class, "/assets/css/style.css", STYLES_PATH);
+
+        final FXMLLoader loginLoader = styleLoader.getFXMLLoaderAndExport(Environment.class, "/assets/login.fxml", STYLES_PATH);
+        final FXMLLoader listLoader = styleLoader.getFXMLLoaderAndExport(Environment.class, "/assets/list.fxml", STYLES_PATH);
+        final FXMLLoader chatLoader = styleLoader.getFXMLLoaderAndExport(Environment.class, "/assets/chat.fxml", STYLES_PATH);
+
+        guiManager.registerGui(LoginController.class, loginLoader, stage);
+        guiManager.registerGui(ListController.class, listLoader, stage);
+        guiManager.registerGui(ChatController.class, chatLoader, stage);
+
+        LoginController loginController = Objects.requireNonNull(guiManager.getGuiPack(LoginController.class)).getThird();
+        loginController.getIpField().setText(Environment.STANDARD_IP);
+
+        guiManager.setAllTransparent();
+        guiManager.setAllDraggable();
+
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setTitle("NChat");
+
+        guiManager.setGui(LoginController.class);
+
+        FadeTransition transition = new FadeTransition();
+        transition.setDuration(Duration.seconds(0.25));
+        transition.setFromValue(0);
+        transition.setToValue(100);
+        transition.setNode(loginController.getPane());
+
+        stage.show();
+        transition.play();
+    }
+
+
+    public static void start(@NotNull final OptionSet optionSet) {
+        launch();
+        getLogger().info("NChat v" + Environment.APP_VERSION + " is launching.");
+    }
+
+    public GuiManager getGuiManager() {
+        return guiManager;
+    }
+
+    public void setUser(@NotNull final User user) {
+        this.user = user;
+    }
+
+    @NotNull
+    public User getUser() {
+        return user;
+    }
+
+    public static Client getClient() {
+        return client;
+    }
+
+    public static Logger getLogger() {
+        return LOGGER;
+    }
+
+}
---
+++ server/src/main/java/com/demkom58/nchat/server/network/ServerPacketHandler.java
@@ -0,0 +1,76 @@
+package com.demkom58.nchat.server.network;
+
+import com.demkom58.nchat.common.Environment;
+import com.demkom58.nchat.common.network.IPacketHandlerRegistry;
+import com.demkom58.nchat.common.network.handler.PacketHandler;
+import com.demkom58.nchat.common.network.util.NetworkUtil;
+import com.demkom58.nchat.server.Server;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+
+public class ServerPacketHandler extends PacketHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger("[SHandler]");
+
+    public ServerPacketHandler(IPacketHandlerRegistry packetProcessorRegistry) {
+        super(packetProcessorRegistry);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        super.handlerAdded(ctx);
+
+        Server server = Server.getServer();
+        Collection<User> users = server.getUsers();
+
+        int connections = 0;
+
+        String address = NetworkUtil.getAddress(ctx.channel()).split(":")[0];
+        Channel incoming = ctx.channel();
+
+        for (User user : users) {
+
+            if (address.equals(user.getIP()))
+                connections += 1;
+
+            if (connections < Environment.CONNECTIONS_PER_IP)
+                continue;
+
+            String reason = "Too many connections.";
+
+            User.sendMessage(incoming, reason);
+            User.sendMessage(incoming, "You was kicked.");
+
+            server.kickUser(incoming, reason);
+            return;
+        }
+
+        server.getChannels().add(incoming);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) {
+        if(!Server.getServer().getChannels().contains(ctx.channel())
+                && !Server.getServer().getRegisteredChannels().contains(ctx.channel()))
+            return;
+
+        Channel channel = ctx.channel();
+        Server server = Server.getServer();
+
+        server.removeUser(channel);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        Channel channel = ctx.channel();
+
+        String reason = "Connection["+ NetworkUtil.getAddress(channel)+"] Exception caught: " + cause.getMessage();
+        LOGGER.warn(reason);
+
+        handlerRemoved(ctx);
+    }
+
+}
