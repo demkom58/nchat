ee67f6ebcd01a77f06c1179efce0fb27b9bc0a1a
DEMKOM58 <demkom58@gmail.com>
2020-02-06 10:52:18 +0200

    Client support of connection to server with custom port

--- client/src/main/java/com/demkom58/nchat/client/util/DataIP.java
+++ client/src/main/java/com/demkom58/nchat/client/util/DataIP.java
@@ -1,6 +1,9 @@
 package com.demkom58.nchat.client.util;
 
 import com.demkom58.nchat.common.Environment;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -14,61 +17,73 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DataIP {
-
-    private static final String SRVS = Environment.DATA_PATH + "srv.list";
+    private static final Logger LOGGER = LoggerFactory.getLogger("DataIP");
+    private static final String SERVER_LIST_FILE_PATH = Environment.DATA_PATH + "srv.list";
 
     public static void saveIP(String ip) {
-        try {
-            File file = new File(SRVS);
-            createFileIfNotExist(file);
+        final File file = createFileIfNotExist(SERVER_LIST_FILE_PATH);
 
+        try {
             List<String> ips = Files.readAllLines(Paths.get(file.getPath()), StandardCharsets.UTF_8);
             BufferedWriter writer = new BufferedWriter(new FileWriter(file));
             boolean exist = ips.contains(ip);
 
-            for(String fip : ips) {
+            for (String fip : ips) {
                 writer.write(fip);
                 writer.newLine();
             }
 
-            if(!exist) writer.write(ip);
+            if (!exist) writer.write(ip);
 
             writer.flush();
             writer.close();
-        } catch (IOException e) { e.printStackTrace(); }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void saveIPList(List<String> ips) {
         try {
-            File file = new File(SRVS);
+            final File file = createFileIfNotExist(SERVER_LIST_FILE_PATH);
+            final FileWriter writer = new FileWriter(file);
 
-            createFileIfNotExist(file);
-            FileWriter writer = new FileWriter(SRVS);
-            for (String str : ips) writer.write(str + "\n");
+            for (String str : ips)
+                writer.write(str + "\n");
+
             writer.close();
-
-        } catch (IOException e) { e.printStackTrace(); }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static List<String> loadIPList() {
         try {
-            File file = new File(SRVS);
-            createFileIfNotExist(file);
-
+            File file = createFileIfNotExist(SERVER_LIST_FILE_PATH);
             List<String> ips = new ArrayList<>();
 
             Scanner scanner = new Scanner(file);
             while (scanner.hasNext()) ips.add(scanner.nextLine());
 
             return ips;
-        } catch (IOException e) { e.printStackTrace(); }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new ArrayList<>();
     }
 
-    private static boolean createFileIfNotExist(File file) {
-        try {
-            if (!file.exists()) return file.createNewFile();
-            return false;
-        } catch (IOException ex) { return false; }
+    private static @NotNull File createFileIfNotExist(@NotNull final String path) {
+        final File file = new File(path);
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                LOGGER.error("Can't create file in path " + path, e);
+                return file;
+            }
+        }
+
+        return file;
+
     }
 }
