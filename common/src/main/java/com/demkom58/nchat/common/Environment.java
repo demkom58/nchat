c7f5c2f82a106455a1d52dac7c1a4a1f3a5b2824
DEMKOM58 <demkom58@gmail.com>
2020-02-06 10:04:42 +0200

    Now project uses gradle sub projects

---
+++ common/src/main/java/com/demkom58/nchat/common/Environment.java
@@ -0,0 +1,20 @@
+package com.demkom58.nchat.common;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Environment {
+    public static final String HOST = "localhost";
+    public static final int PORT = 55555;
+
+    public static final String PROTOCOL_VERSION = "1.2b4";
+    public static final String STYLING_VERSION = "1.1";
+    public static final String APP_VERSION = "1.1 - SNAPSHOT";
+
+    public static final int MAX_MESSAGE_LENGTH = 1000;
+    public static final String STANDARD_IP = "localhost";
+
+    public static final int CONNECTIONS_PER_IP = 3;
+    public static final int MESSAGES_PER_SECOND = 3;
+    public static final String DATA_PATH = System.getenv("APPDATA") + "/NChat/";
+}
--- src/main/java/com/demkom58/nchat/Main.java
+++ src/main/java/com/demkom58/nchat/Main.java
@@ -2,32 +2,13 @@ package com.demkom58.nchat;
 
 import com.demkom58.nchat.client.Client;
 import com.demkom58.nchat.server.Server;
-import io.netty.util.internal.logging.InternalLoggerFactory;
-import io.netty.util.internal.logging.Slf4JLoggerFactory;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
 import java.util.Arrays;
 
 public class Main {
-    public static final String HOST = "localhost";
-    public static final int PORT = 55555;
-
-    public static final String PROTOCOL_VERSION = "1.2b4";
-    public static final String STYLING_VERSION = "1.1";
-    public static final String APP_VERSION = "1.1 - SNAPSHOT";
-
-    public static final int MAX_MESSAGE_LENGTH = 1000;
-    public static final String STANDARD_IP = "localhost";
-
-    public static final int CONNECTIONS_PER_IP = 3;
-    public static final int MESSAGES_PER_SECOND = 3;
-    public static final String DATA_PATH = System.getenv("APPDATA") + "/NChat/";
-    public static final String STYLES_PATH = DATA_PATH + "styles/";
-
     public static void main(String[] args) throws Exception {
-        InternalLoggerFactory.setDefaultFactory(Slf4JLoggerFactory.INSTANCE);
-
         final OptionParser optionParser = new OptionParser();
 
         optionParser.acceptsAll(Arrays.asList("s", "server"));
